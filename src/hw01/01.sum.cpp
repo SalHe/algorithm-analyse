825d0a0b036c08d3414aa386e26e56e4c2edf314
SalHe <SalHe@qq.com>
2021-09-17 18:23:55 -0700

    增加结果输出

--- src/hw01/01.sum.cpp
+++ src/hw01/01.sum.cpp
@@ -2,48 +2,110 @@
 #include <doctest.h>
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <tuple>
 
 using namespace std;
+using Ints = vector<int>;
+using Combinations = vector<Ints>;
+using Result = tuple<int, Combinations>;
 
 #define NOT_FOUND 0
 
-int findSumCombination(vector<int> &nums, int sum, vector<int> &curr, int numsPos)
+void findSumCombination(Ints &nums, int sum, Ints &curr, int numsPos, Combinations &combinations, int &max)
 {
     int currSum = 0;
     for_each(curr.begin(), curr.end(),
              [&](int x)
              { currSum += x; });
-    if (currSum == sum)
-        return curr.size();
+    if (currSum > sum)
+    {
+        return;
+    }
+    else if (currSum == sum)
+    {
+        if (curr.size() > max)
+        {
+            max = curr.size();
+            combinations.clear();
+            combinations.push_back(curr);
+        }
+        else if (curr.size() == max)
+        {
+            combinations.push_back(curr);
+        }
+        return;
+    }
     if (numsPos >= nums.size())
-        return NOT_FOUND;
+        return;
 
     for (int i = numsPos; i < nums.size(); i++)
     {
+        // TODO 考虑如何去重
+        // if (numsPos && nums[i] == curr[numsPos - 1])
+        //     continue;
         curr.push_back(nums[i]);
-        int ret = findSumCombination(nums, sum, curr, numsPos + 1);
+        findSumCombination(nums, sum, curr, i + 1, combinations, max);
         curr.pop_back();
-        if (ret)
-            return ret;
     }
-    return NOT_FOUND;
 }
 
-int findSumCombination(vector<int> nums, int sum)
+Result findSumCombination(Ints nums, int sum)
 {
     // 首先对给定的元素排序，
     // 具体排序算法暂不考
     sort(nums.begin(), nums.end());
 
-    vector<int> curr;
+    Ints curr;
+    Combinations combinations;
+    int max = 0;
 
-    return findSumCombination(nums, sum, curr, 0);
+    findSumCombination(nums, sum, curr, 0, combinations, max);
+    return {max, combinations};
 }
 
 TEST_CASE("求和")
 {
-    CHECK_EQ(findSumCombination({1, 2, 3, 4}, 6), /* result: */ 3);
-    CHECK_EQ(findSumCombination({4, 2, 1, 3}, 6), /* result: */ 3);
-    CHECK_EQ(findSumCombination({3, 3, 1, 2}, 6), /* result: */ 3);
-    CHECK_EQ(findSumCombination({1, 1, 1, 3, 2}, 6), /* result: */ 4);
+    CHECK_EQ(
+        findSumCombination({1, 2, 3, 4}, 6),
+
+        (Result){
+            /* 序列中数字个数 */ 3,
+            /*可能的结果集，遵循字典序*/
+            {
+                {1, 2, 3},
+            },
+        });
+    CHECK_EQ(
+        findSumCombination({4, 2, 1, 3}, 6),
+
+        (Result){
+            /* 序列中数字个数 */ 3,
+            /*可能的结果集，遵循字典序*/
+            {
+                {1, 2, 3},
+            },
+        });
+    CHECK_EQ(
+        findSumCombination({3, 3, 1, 2}, 6),
+
+        (Result){
+            /* 序列中数字个数 */ 3,
+            /*可能的结果集，遵循字典序*/
+            {
+                {1, 2, 3},
+                {1, 2, 3},
+            },
+        });
+    CHECK_EQ(
+        findSumCombination({1, 1, 1, 3, 2}, 6),
+
+        (Result){
+            /* 序列中数字个数 */ 4,
+            /*可能的结果集，遵循字典序*/
+            {
+                {1, 1, 1, 3},
+            },
+        });
+
 }
