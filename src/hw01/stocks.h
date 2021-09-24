0e8fc806eadd5d35633b408909b7f80841ded61e
SalHe <SalHe@qq.com>
2021-09-24 09:07:29 -0700

    hw01-3.更新股票代码分析

--- src/hw01/stocks.h
+++ src/hw01/stocks.h
@@ -5,6 +5,8 @@ using namespace std;
 
 tuple<double, int, int> predictStock(vector<double> prices)
 {
+    // 思路分析已放入[src/hw01/README.md]中
+
     double max = 0, left = 0, right = 0;
     int tryStart = 0;
     for (int i = 1; i < prices.size(); i++)
