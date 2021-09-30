5e641f723cbf1bc9c09231b83093618ddacfe64a
SalHe <SalHe@qq.com>
2021-09-29 21:11:48 -0700

    hw02——3.基于位运算寻找出现次数超过一半的元素

--- src/hw02/03.frequent_number.cpp
+++ src/hw02/03.frequent_number.cpp
@@ -15,6 +15,12 @@ using Result = std::tuple<bool, Number>;
 const Result NOT_FOUND = {false, 0};
 #define FOUND(x) ((Result){true, x})
 
+/**
+ * @brief 查找出现次数超过一半的数。摩尔投票算法。
+ * 
+ * @param numbers 
+ * @return Result 
+ */
 Result findFrequentNumber(Numbers numbers)
 {
     if (numbers.size() <= 0)
@@ -45,8 +51,81 @@ Result findFrequentNumber(Numbers numbers)
     return NOT_FOUND;
 }
 
+/**
+ * @brief 查找出现次数超过一半的数。位运算法。
+ *
+ * 个人感觉这个算法设计得很精巧，但是不如摩尔投票法。
+ * 
+ * 参考：https://blog.csdn.net/qq_33880027/article/details/90764752
+ * 
+ * 该参考文档中的代码存在一些问题，已做出修正：
+ * 1. 无法解决目标数为0的情况。
+ * 2. 无法描述解的情况（是否有解）。
+ * 3. 原给定的结果可能不是解
+ * 
+ * @param numbers 
+ * @return Result 
+ */
+Result findFrequentNumber2(std::vector<int> numbers)
+{
+    std::vector<int> bits(32, 0);
+    int res = 0;
+    int countForZero = 0;
+    for (int i : numbers)
+    {
+        if (i == 0)
+        {
+            countForZero++;
+            continue;
+        }
+        int mask = 0x1;
+        for (int j = 0; j < 32; j++)
+        {
+            bits[j] += (i & mask) >> j;
+            mask <<= 1;
+        }
+    }
+    bool found = false;
+    if (countForZero > numbers.size() / 2)
+    {
+        return FOUND(0);
+    }
+    for (int i = 0; i < 32; i++)
+    {
+        if (bits[i] > numbers.size() / 2)
+        {
+            res += 1 << i;
+            found = true;
+        }
+    }
+    int count = 0;
+    for (auto &&i : numbers)
+    {
+        if (i == res)
+            count++;
+    }
+
+    if (!found || count < numbers.size() / 2)
+        return NOT_FOUND;
+    return FOUND(res);
+}
+
 TEST_CASE("寻找出现次数超过一半的数")
 {
-    CHECK_EQ(findFrequentNumber({3, 1, 1, 2, 1, 2, 1, 1}), FOUND(1));
-    CHECK_EQ(findFrequentNumber({3, 1, 1, 2, 3, 2}), NOT_FOUND);
+    typedef struct
+    {
+        std::vector<int> numbers;
+        Result expect;
+    } TestCase;
+    std::vector<TestCase> testCases = {
+        {{3, 1, 1, 2, 1, 2, 1, 1}, FOUND(1)},
+        {{3, 1, 1, 2, 3, 2}, NOT_FOUND},
+        {{0, 2, 0, 0, 1, 0, 1, 0, 1, 1, 0, 3, 0}, FOUND(1)},
+    };
+
+    for (auto &&testCase : testCases)
+    {
+        CHECK_EQ(findFrequentNumber(testCase.numbers), testCase.expect);
+        CHECK_EQ(findFrequentNumber2(testCase.numbers), testCase.expect);
+    }
 }
